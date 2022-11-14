110d8cd197076c8d4ae70ce87e27685361d376cb
Alexander Zakharov <sys@eml.ru>
2022-11-15 01:10:33 +0300

    added tests config

--- src/test/java/com/example/springjpatest/SpringJpaTestApplicationTests.java
+++ src/test/java/com/example/springjpatest/SpringJpaTestApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.springjpatest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = {"production", "test"})
 class SpringJpaTestApplicationTests {
 
     @Test
